29b11a38d86dfca3b4d620f2e492c91614e69e0b
3garfield5 <139701743+3garfield5@users.noreply.github.com>
2023-10-18 13:29:15 +0300

    Add files via upload

---
+++ MyWebApp/js/script.js
@@ -0,0 +1,33 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+  function myFunc() {
+    document.getElementById("myDropdn").classList.toggle("show");
+  }
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.drbtn')) {
+      var dropdowns = document.getElementsByClassName("drpdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+  
